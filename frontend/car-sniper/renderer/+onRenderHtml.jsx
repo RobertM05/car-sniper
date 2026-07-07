9af71556366f31b9974aa3320d52c04eceaf1651
Robert Musoiu <robert.musoiu05@gmail.com>
2026-07-07 15:34:54 +0300

    fix: add SVG favicon, fix Vercel SSR routing, add favicon link tags

--- frontend/car-sniper/renderer/+onRenderHtml.jsx
+++ frontend/car-sniper/renderer/+onRenderHtml.jsx
@@ -29,6 +29,7 @@ export default function onRenderHtml(pageContext) {
         ${dangerouslySkipEscape(helmet.script.toString())}
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     </head>
     <body>
